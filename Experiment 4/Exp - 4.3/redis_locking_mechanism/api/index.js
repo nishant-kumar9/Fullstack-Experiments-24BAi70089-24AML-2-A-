3cf89643b6665505452427804426aceddfde05da
Nishant Kumar <nishantkumar06268@gmail.com>
2026-03-05 01:01:54 +0530

    Exp 5 Pushed

---
+++ Experiment 4/Exp - 4.3/redis_locking_mechanism/api/index.js
@@ -0,0 +1,13 @@
+import app from "../src/app.js";
+import { connectRedis } from "../src/config/redis.js";
+
+let redisConnected = false;
+
+export default async function handler(req, res) {
+  if (!redisConnected) {
+    await connectRedis();
+    redisConnected = true;
+  }
+
+  return app(req, res);
+}   
--- Experiment 4/Exp - 4.3/redis_locking_mechanism/index.js
+++ Experiment 4/Exp - 4.3/redis_locking_mechanism/index.js
@@ -22,9 +22,7 @@ const PORT = 3000;
 const startServer = async () => {
     await connectRedis();
 
-    app.listen(PORT, () => {
-        console.log(`🚀 Server running on port ${PORT}`);
-    });
+    
 };
 
 startServer();
