f52dd4bd638db75e99465e5d3b6141d4fbff8398
Nishant Kumar <nishantkumar06268@gmail.com>
2026-04-14 23:14:41 +0530

    Delete Experiment8 directory

--- Experiment8/Experiment-8-main/frontend/src/components/AdminRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-function AdminRoute({ children }) {
-  const user = JSON.parse(localStorage.getItem("user"));
-
-  return user?.role === "admin" ? children : <Navigate to="/" />;
-}
-
-export default AdminRoute;
